e7a898b68273a24aa64ec268572fe2bc336e322f
Benjamin Lewis <benjamin.lewis@talkpush.com>
2020-11-01 15:14:25 -0600

    master | Add listeners and stream url to strings file

--- app/src/main/java/com/tabeelcr/lib_vlx_android_test/MainActivity.java
+++ app/src/main/java/com/tabeelcr/lib_vlx_android_test/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.Uri;
 import android.os.Bundle;
-import android.webkit.WebView;
 
 import org.videolan.libvlc.LibVLC;
 import org.videolan.libvlc.Media;
@@ -17,9 +16,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private static final boolean USE_TEXTURE_VIEW = false;
-    private static final boolean ENABLE_SUBTITLES = true;
-    private static final String ASSET_FILENAME = "bbb.m4v";
-
+    private static final boolean ENABLE_SUBTITLES = false;
     private VLCVideoLayout mVideoLayout = null;
 
     private LibVLC mLibVLC;
@@ -30,13 +27,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ArrayList<String> args = new ArrayList<>();
-        args.add("--aout=opensles");
-        args.add("--audio-time-stretch"); // time stretching
-        args.add("-vvv"); // verbosity
+        final ArrayList<String> options = new ArrayList<>();
+        options.add("--aout=opensles");
+        options.add("--audio-time-stretch"); // time stretching
+        options.add("-vvv"); // verbosity
 
-        mLibVLC = new LibVLC(this, args);
+        mLibVLC = new LibVLC(this, options);
         mMediaPlayer = new MediaPlayer(mLibVLC);
+        // mMediaPlayer.setEventListener(getListener());
 
         mVideoLayout = findViewById(R.id.video_layout);
     }
@@ -51,25 +49,52 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-
-        mMediaPlayer.attachViews(mVideoLayout, null, ENABLE_SUBTITLES, USE_TEXTURE_VIEW);
-
-        try {
-            final Media media = new Media(mLibVLC, Uri.parse("http://k3.usastreams.com/CableLatino/88stereo/playlist.m3u8"));
-            mMediaPlayer.setMedia(media);
-            media.release();
-        } catch (Exception e) {
-            throw new RuntimeException("Invalid asset folder");
-        }
-        mMediaPlayer.play();
+        play();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        cancel();
+    }
 
+    private MediaPlayer.EventListener getListener () {
+        return new MediaPlayer.EventListener() {
+            @Override
+            public void onEvent(MediaPlayer.Event event) {
+                switch (event.type) {
+                    case MediaPlayer.Event.Buffering:
+                    case MediaPlayer.Event.EncounteredError:
+                        // Replay the stream
+                        // Logic to replay if buffering is taking too long.
+                        cancel();
+                        play();
+                        break;
+                    case MediaPlayer.Event.EndReached:
+                        cancel();
+                        break;
+                }
+            }
+        };
+    }
+
+    private void cancel() {
         mMediaPlayer.stop();
+        mMediaPlayer.release();
+        mLibVLC.release();
         mMediaPlayer.detachViews();
+    }
+
+    private void play() {
+        final String streamUrl = getString(R.string.stream_url);
+        final Media media = new Media(mLibVLC, Uri.parse(streamUrl));
+
+        mMediaPlayer.attachViews(mVideoLayout, null, ENABLE_SUBTITLES, USE_TEXTURE_VIEW);
+        mMediaPlayer.setMedia(media);
+
+        media.release();
+
+        mMediaPlayer.play();
     }
 
 }
